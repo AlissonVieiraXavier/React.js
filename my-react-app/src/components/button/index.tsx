623d33ff8169754b101e65b1ad08b6b28179c29c
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-22 19:09:48 -0300

    a

--- my-react-app/src/components/button/index.tsx
+++ my-react-app/src/components/button/index.tsx
@@ -2,8 +2,14 @@ import react from 'react';
 
 class Botao extends react.Component{
     render(){
+        const estaAtivo = true;
+        const styles = {
+            backgroundColor: estaAtivo ? "#338" : "black",
+            color: 'white'
+        }
+
         return(
-            <button>            
+            <button style={styles}>            
                 Clique Aqui
             </button>
         )
