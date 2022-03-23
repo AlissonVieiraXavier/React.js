ce1bd5442bdc0bbc2693a7dc680d15f97bd8da80
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-23 12:21:16 -0300

    a

--- my-react-app/src/components/Lista/index.tsx
+++ my-react-app/src/components/Lista/index.tsx
@@ -1,39 +1,32 @@
 import React from "react";
-import style from './Lista.module.scss'
-import Item from './item'
+import style from "./Lista.module.scss";
+import Item from "./item";
 
-function Lista(){
-    const tarefas = [
-        {
-           tarefa:'react',
-           tempo:'02:00:00' 
-        },
-        {
-            tarefa:'javascript',
-            tempo: '01:00:00'
-        },
-        {
-            tarefa:'typescript',
-            tempo:'03:05:01'
-        }
-    ]
-    return(
-        <aside className={style.listaTarefas}>
-         <h2> Estudos do dia </h2>
-         <ul>
-             {tarefas.map((item, index) => (
-               <Item
-               tarefa = {item.tarefa}
-               tempo = {item.tempo}
-               />
-                  
-               
-             )
-             )} 
-         </ul>
-         
-        </aside>
-    )
+function Lista() {
+  const tarefas = [
+    {
+      tarefa: "react",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "javascript",
+      tempo: "01:00:00",
+    },
+    {
+      tarefa: "typescript",
+      tempo: "03:05:01",
+    },
+  ];
+  return (
+    <aside className={style.listaTarefas}>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <Item key={index} tarefa={item.tarefa} tempo={item.tempo} />
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 export default Lista;
