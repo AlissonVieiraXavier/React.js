ccfa505c12a65f902c05cd508e80a7f0f883bc30
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-28 18:09:15 -0300

    a

---
+++ portifolio/src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+//import style from './Navbar.module.scss'
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar"></div>
+
+    </>
+  )
+}
+
+export default Navbar;
