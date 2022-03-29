2ff09461d6b93d8d4191e8564de7f7968becf919
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-29 09:46:58 -0300

    a

--- portifolio/src/components/Navbar/index.tsx
+++ portifolio/src/components/Navbar/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import './Navbar.css'
 import { FaBars } from 'react-icons/fa';
+const perfil = require('./img/perfil.webp') 
+
 
 
 function Navbar() {
   return (
     <>
-      <div className="navbar">
-    
-      <FaBars/>
+      <div className='navbar'>
+      <a href='index.html'className="menu-bar"><FaBars/></a>
+      <img src={perfil} className="menu-img"></img>
+      <h1> shit</h1>
       
       </div>
 
