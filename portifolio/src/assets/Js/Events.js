40d4a5401e2430b6ff0e9883fcf0718ad4420dca
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-29 11:25:34 -0300

    s

---
+++ portifolio/src/assets/Js/Events.js
@@ -0,0 +1,6 @@
+let menuBar = document.querySelector(".menu-bar");
+let navOptions = document.querySelector(".nav-options");
+menuBar.addEventListener('click',() => {
+    //navOptions.classList.toggle('navOptions-active');
+    console.log('nheee')
+})
--- portifolio/src/components/Navbar/index.tsx
+++ portifolio/src/components/Navbar/index.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './Navbar.css'
 import { FaBars } from 'react-icons/fa';
+import { BsBook} from 'react-icons/bs';
+import { FaPencilAlt } from 'react-icons/fa';
+import { BsEmojiSunglasses } from 'react-icons/bs';
+import { HiOutlinePhone } from 'react-icons/hi';
 const perfil = require('./img/perfil.webp') 
-
 
 
 function Navbar() {
   return (
     <>
       <div className='navbar'>
-      <a href='index.html'className="menu-bar"><FaBars/></a>
+      <span className="menu-bar"><FaBars/></span>
+      
       <img src={perfil} className="menu-img"></img>
-      <h1> shit</h1>
+      
       
       </div>
-
+      <div className='nav-options'>
+        <ul className='nav-options-ul'>
+          <li className='nav-options-li'><BsBook className="icons-nav"/>  Portifolio</li>
+          <li className='nav-options-li'><FaPencilAlt className="icons-nav" />Habilidades</li>
+          <li className='nav-options-li'><BsEmojiSunglasses className="icons-nav" />Sobre mim</li>
+          <li className='nav-options-li'><HiOutlinePhone className="icons-nav"/> Contato</li>
+        </ul>
+      </div>
     </>
   )
 }
