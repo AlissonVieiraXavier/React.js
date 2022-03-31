ad1539d80f7b3c788bc29d50c16b9fb50a18072a
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-31 15:04:20 -0300

    a

---
+++ portifolio/src/components/Section1/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Section1.css';
+import { BiCodeAlt } from 'react-icons/bi';
+//const perfil = require('./img/perfil.webp');
+//<img src={perfil} className="menu-img"></img> 
+
+
+function Section1() {
+
+ 
+  return (
+    <div className="section-main">
+     <figure><BiCodeAlt/></figure>
+     <h1 className="h1section1">localizado gfndzfn</h1>
+    </div>
+  )
+}
+
+export default Section1;
--- portifolio/src/pages/App.tsx
+++ portifolio/src/pages/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Section1 from '../components/Section1';
 //import Navbar from '../components/Navbar';
 
 export default function App() {
   return (
     <div>
       <Navbar/>
+      <Section1/>
     </div>
   )
 }
