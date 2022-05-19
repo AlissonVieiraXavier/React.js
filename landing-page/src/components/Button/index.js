4f19ddcbb2d45bdec55e855d8c8dcc26a2f1af85
AlissonVieiraXavier <alissonvieiraxavier@hotmail.com>
2022-05-19 11:30:31 -0300

    a

--- landing-page/src/components/Button/index.js
+++ landing-page/src/components/Button/index.js
@@ -1,9 +1,9 @@
 import styles from './index.module.css'
 
-export default function Button({titulo}){
+export default function Button({titulo,icon}){
     return( 
         <>
-        <button className={styles.header_bottom_div_texto_button}>{titulo}</button>
+        <button className={styles.header_bottom_div_texto_button}>{titulo}<i>{icon}</i></button>
         </>
     )
 }
--- landing-page/src/components/Header/HeaderBottom/index.js
+++ landing-page/src/components/Header/HeaderBottom/index.js
@@ -1,5 +1,6 @@
 import Button from '../../Button';
 import styles from './index.module.css'
+import {MdChair} from 'react-icons/md'
 
 
 export default function HeaderBottom({ img, alt }) {
@@ -9,8 +10,8 @@ export default function HeaderBottom({ img, alt }) {
       <div className={styles.header_bottom_div_texto}>
           <h1 className={styles.header_bottom_div_texto_title}>Seminário de Marketing Digital com Camila Freitas</h1>
           <p className={styles.header_bottom_div_texto_p}> Descubra os segredos do marketing digital</p>
-          <div className="header_bottom_div_texto_button"></div>
-          <div> <Button titulo="Reservar meu lugar"/></div>
+          <div className={styles.header_bottom_div_texto_button1}><Button titulo="Reservar meu lugar" icon={<MdChair/>}/></div>
+
       </div>
       <div className={styles.header_bottom_div_img}>
         <img className={styles.img}src={img} alt={alt} />
