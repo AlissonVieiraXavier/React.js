0cef34e526f195fcbf59b6f549b9b663ba515828
AlissonVieiraXavier <alissonvieiraxavier@hotmail.com>
2022-05-20 06:35:34 -0300

    a

--- landing-page/src/components/Animacao/index.js
+++ landing-page/src/components/Animacao/index.js
@@ -5,7 +5,7 @@ import mktpng from './15.png'
 export default function Animacao(){
     return( 
         <div className={styles.animacao}>
-        <h2 className={styles.animacao_title}> Entenda como melhorar suas campanhas e atingir seu publico-alvo.</h2>
+        <h2 className={styles.animacao_title}> Entenda como melhorar suas campanhas e atingir <br></br>seu <strong className={styles.strong}>publico-alvo!</strong></h2>
         <img className={styles.animacao_img} src={mktpng} alt="animação"/>
    
         </div>
