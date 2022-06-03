58d86bec6174b24ce7d8e08926b5bd2b3b4bb491
AlissonVieiraXavier <alissonvieiraxavier@hotmail.com>
2022-06-03 18:14:01 -0300

    conclusão do header

--- cardapiou/src/pages/Cardapio/index.tsx
+++ cardapiou/src/pages/Cardapio/index.tsx
@@ -1,13 +1,20 @@
 import styles from './cardapio.module.scss';
-import logo from 'assets/img/logo.png'
+import logo from 'assets/img/logo.png';
+import {GiChickenOven} from 'react-icons/gi';
 
 export default function Cardapio(){
     return(
         <main>
             <nav className={styles.menu}>
               <img className={styles.img} src={logo} alt="logo do flangou"/>
-              <h1 className={styles.title} >Flangou Restaurant</h1>
+              <h1 className={styles.title}>Flangou Restaurant</h1>
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    Especialidade em Frangos <i><GiChickenOven/></i>
+                </div>
+
+            </header>
         </main>
     )
 }
