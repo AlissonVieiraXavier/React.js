3f473baaf4b82087b2ec6d7d6b9279779923b2e0
Alisson Xavier <78052229+AlissonVieiraXavier@users.noreply.github.com>
2022-06-20 12:45:49 -0300

    ajuste navbar

--- canalcac/src/Components/Navbar/index.js
+++ canalcac/src/Components/Navbar/index.js
@@ -5,23 +5,29 @@ import Sobre from '../../Pages/Sobre';
 import MaisVistos from '../../Pages/MaisVistos';
 import Contato from '../../Pages/Contato';
 import logo from '../../assets/logo.png';
-import Togglebotton from '../ToggleBotton';
+import {GoThreeBars} from 'react-icons/go';
+import {useState} from 'react';
 
 export default function Navbar(){
+
+    const [notActive,setActive] = useState(true);
+
+    const ToggleToActive = () => (
+        setActive(!notActive)
+    )
+
     return(
     <nav className={styles.navbar}>
         <div className={styles.responsive_mobile}>
             <img src={logo} alt='Logo' class={styles.logotipo}/>
-            <div className={styles.Togglebotton}>
-                <Togglebotton/>
-            </div>
+             <button  onClick={ToggleToActive} className={styles.btn_burger}> <GoThreeBars/> </button>
         </div>
         <BrowserRouter>
-        <ul className={styles.list}>
-            <li className={styles.list_item}><Link to="/">Pagina Inicial</Link></li>
-            <li className={styles.list_item}><Link to="/Sobre">Sobre</Link></li>
-            <li className={styles.list_item}><Link to="/Mais-vistos">Mais Vistos</Link></li>
-            <li className={styles.list_item}><Link to="/Contato">Contato</Link></li>
+        <ul className={notActive ? styles.list : styles.list_active}>
+            <li className={styles.list_item}><Link className={styles.list_item} to="/">Pagina Inicial</Link></li>
+            <li className={styles.list_item}><Link className={styles.list_item} to="/Sobre">Sobre</Link></li>
+            <li className={styles.list_item}><Link className={styles.list_item} to="/Mais-vistos">Mais Vistos</Link></li>
+            <li className={styles.list_item}><Link className={styles.list_item} to="/Contato">Contato</Link></li>
         </ul>
         <Routes>
             <Route exact path='/' element={<PaginaInicial/>}></Route>
--- canalcac/src/Components/ToggleBotton/index.js
+++ canalcac/src/Components/ToggleBotton/index.js
@@ -1,12 +1,11 @@
-import styles from './ToggleBotton.module.css';
+
+
 
 export default function Togglebotton(){
-        return(
-        <div className={styles.burger_seems}>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
+
+    return(
+    
+<></>
         )
 
 }
